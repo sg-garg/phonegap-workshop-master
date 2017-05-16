df34f0492689108c40bd84048f7534ed231f2a7d
LAPTOP-FGQCT0AB\shara <scs.garg@gmail.com>
2017-05-16 23:35:56 +0530

    sdfsf

--- js/main.js
+++ js/main.js
@@ -1,27 +1,45 @@
 var app = {
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	// renderHomeView: function() {
+		// var html =
+				// "<div class='header'><h1>Home</h1></div>" +
+				// "<div class='search-view'>" +
+				// "<input class='search-key'/>" +
+				// "<ul class='employee-list'></ul>" +
+				// "</div>"
+		// $('body').html(html);
+		// $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	// },
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+    // findByName: function() {
+        // console.log('findByName');
+        // this.store.findByName($('.search-key').val(), function(employees) {
+            // var l = employees.length;
+            // var e;
+            // $('.employee-list').empty();
+            // for (var i=0; i<l; i++) {
+                // e = employees[i];
+                // $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            // }
+        // });
+    // },
+	
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
 
     initialize: function() {
-        // this.store = new MemoryStore();
-        // $('.search-key').on('keyup', $.proxy(this.findByName, this));
-			
 		var self = this;
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 		this.store = new MemoryStore(function() {
-			self.showAlert('Store Initialized', 'Info');
+			self.renderHomeView();
 		});
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));	
 	}
 	,
 	showAlert: function (message, title) {
