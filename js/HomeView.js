83027549abcb5acddb45462859c1d745c87bfc43
LAPTOP-FGQCT0AB\shara <scs.garg@gmail.com>
2017-05-18 08:20:58 +0530

    sss

--- js/HomeView.js
+++ js/HomeView.js
@@ -9,11 +9,26 @@ var HomeView = function(store) {
 		return this;
 	};
 	
+	// this.findByName = function() {
+	// 	store.findByName($('.search-key').val(), function(employees) {
+	// 		$('.employee-list').html(HomeView.liTemplate(employees));
+	// 	});
+	// };
+
 	this.findByName = function() {
 		store.findByName($('.search-key').val(), function(employees) {
 			$('.employee-list').html(HomeView.liTemplate(employees));
+			if (self.iscroll) {
+				console.log('Refresh iScroll');
+				self.iscroll.refresh();
+			} else {
+				console.log('New iScroll');
+				self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+			}
 		});
 	};
+	
+
 	
 	
 	
